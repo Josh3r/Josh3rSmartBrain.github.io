26cb51663eb7bb4ec046b5b5c4129f89f004e690
Test <teo.josh@yahoo.com.sg>
2019-11-18 14:10:10 +0800

    Updates

---
+++ precache-manifest.6fd26c0bb0ffe5692c91455e60bd7089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cfbd8db1f7c33a3552afe5412925e5",
+    "url": "/imagedetector/index.html"
+  },
+  {
+    "revision": "e2a3613611cc5a51ceab",
+    "url": "/imagedetector/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2d7503c4d135a460bcdb",
+    "url": "/imagedetector/static/css/main.4b2f225c.chunk.css"
+  },
+  {
+    "revision": "e2a3613611cc5a51ceab",
+    "url": "/imagedetector/static/js/2.3799fe62.chunk.js"
+  },
+  {
+    "revision": "2d7503c4d135a460bcdb",
+    "url": "/imagedetector/static/js/main.4ca53dc7.chunk.js"
+  },
+  {
+    "revision": "6a80c8c35ea37668eaf6",
+    "url": "/imagedetector/static/js/runtime-main.10be2ee7.js"
+  }
+]);
